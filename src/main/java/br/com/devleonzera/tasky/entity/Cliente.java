98a24bcdd2352b6146b0adf2b857585f6a56e93a
Leonardo Andrade <leonandrade22@gmail.com>
2024-01-14 23:44:17 -0300

    Adicionando o restante dos atributos do Cliente na classe de entidade

--- src/main/java/br/com/devleonzera/tasky/entity/Cliente.java
+++ src/main/java/br/com/devleonzera/tasky/entity/Cliente.java
@@ -1,17 +1,22 @@
 package br.com.devleonzera.tasky.entity;
 
 import jakarta.persistence.*;
+import java.time.LocalDate; // Importe a classe LocalDate
 
 @Entity
 @Table(name = "clientes")
-public class Cliente{
+public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nome;
     private String email;
+    private String tipo;
     private boolean ativo;
+    private LocalDate dataContrato;
 
+    private int diaVencimento;
+    private boolean mensalista;
     public long getId() {
         return id;
     }
@@ -42,5 +47,37 @@ public class Cliente{
 
     public void setAtivo(boolean ativo) {
         this.ativo = ativo;
+    }
+
+    public LocalDate getDataContrato() {
+        return dataContrato;
+    }
+
+    public void setDataContrato(LocalDate dataContrato) {
+        this.dataContrato = dataContrato;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getDiaVencimento() {
+        return diaVencimento;
+    }
+
+    public void setDiaVencimento(int diaVencimento) {
+        this.diaVencimento = diaVencimento;
+    }
+
+    public boolean isMensalista() {
+        return mensalista;
+    }
+
+    public void setMensalista(boolean mensalista) {
+        this.mensalista = mensalista;
     }
 }
